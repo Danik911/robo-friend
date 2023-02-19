00e38cc813a61e2f2439d5bc6ba57b0b28a72ea2
Daniil Vladimirov <porvipop@mail.ru>
2023-02-19 21:14:05 +0300

    Add UsersApi and Scroll

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import CardList from "./CardList"
-import { robots } from "./robots"
 import SearchBox from "./SearchBox"
 import React, { Component } from "react"
+import "./App.css"
+import Scroll from "./Scroll"
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ""
         }
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
+    }
+
     onSearchChange = (event) => {
         this.setState({ searchField: event.target.value })
     }
@@ -22,9 +30,11 @@ class App extends Component {
         })
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
             </div>
         )
     }
