810c603f3ea5717ef744cf8219cff825401f6fff
Daniil Vladimirov <porvipop@mail.ru>
2023-02-19 18:34:59 +0300

    Add search field

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import CardList from "./CardList"
+import { robots } from "./robots"
+import SearchBox from "./SearchBox"
+import React, { Component } from "react"
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ""
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase())
+        })
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        )
+    }
+}
+
+export default App
